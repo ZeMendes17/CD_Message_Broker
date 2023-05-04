c52f78c734d226a4aed04b538b7470c570d0de25
Ze Mendes <zepedrosmendes@gmail.com>
2023-05-04 19:31:58 +0100

    broker filled, have to test

--- src/broker.py
+++ src/broker.py
@@ -2,6 +2,9 @@
 import enum
 from typing import Dict, List, Any, Tuple
 import socket
+import selectors
+from .protocol import Protocol
+
 
 
 class Serializer(enum.Enum):
@@ -20,13 +23,70 @@ class Broker:
         self.canceled = False
         self._host = "localhost"
         self._port = 5000
-        self._topics = {}
-        self.subscribers = {}
+        self._topics = {} # topic -> value
+        self.subscribers = {} # topic -> [(client, serialization),...]
+        self.socketSerialization = {} # socket -> Serialization (JSON, XML, PICKLE)
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.socket.bind((self._host, self._port))
+        self.socket.listen(100)
+        self.selector = selectors.DefaultSelector()
+        self.selector.register(self.socket, selectors.EVENT_READ, self.accept)
+
+
+    def accept(self, sock, mask):
+        conn, addr = sock.accept()                                  
+        print('accepted', conn, 'from', addr)
+        conn.setblocking(False)
+        self.sel.register(conn, selectors.EVENT_READ, self.read)
+
+
+    def read(self, conn, mask):
+        message = Protocol.recv_msg(conn)
+
+        if message:
+            print('Message received: ', message)
+            msgCommand = message.command
+
+            if msgCommand == 'type': #SerializationMessage
+                print(conn, " is now registered")
+                code = message.code
+                if code == Serializer.JSON:
+                    self.socketSerialization[conn] = Serializer.JSON
+                elif code == Serializer.XML:
+                    self.socketSerialization[conn] = Serializer.XML
+                elif code == Serializer.PICKLE:
+                    self.socketSerialization[conn] = Serializer.PICKLE
+                
+            elif msgCommand == 'subscribe': #SubMessage
+                print(conn, " has subbed to ", message.topic)
+                self.subscribe(message.topic, conn, self.socketSerialization[conn])
+
+            elif msgCommand == 'publish': #PubMessage
+                print(conn, " published", message.topic, " --> ", message.value)
+                self.put_topic(message.topic, message.value)
+                # has to send to subscribers
+                for sub in self.list_subscriptions(message.topic):
+                    Protocol.send_msg(sub[0], message, sub[1])
+
+            elif msgCommand == 'ask': #AskListMessage
+                print("Sending list of topics to ", conn)
+                Protocol.send_msg(conn, Protocol.list(self.list_topics()), self.socketSerialization[conn])
+
+            elif msgCommand == 'cancel': #CancelMessage
+                print(conn, " has cancelled the subscription to ", message.topic)
+                self.unsubscribe(message.topic, conn)
+
+
+        else:
+            print(conn, " disconnected")
+            
+            self.selector.unregister(conn)
+            conn.close()
+
 
 
     def list_topics(self) -> List[str]:
         """Returns a list of strings containing all topics containing values."""
-        #TODO - verificate if this is correct and complete
         list = []
         for key in self._topics:
             list.append(key)
@@ -36,47 +96,51 @@ class Broker:
 
     def get_topic(self, topic):
         """Returns the currently stored value in topic."""
-        #TODO - verificate if this is correct
         # _topics é um dicionario de topicos que contem valor, 
         # ou seja se o topic estiver no dicionario tem algo publicado nele
 
-        return self._topics[topic]
+        if topic in self._topics:
+            return self._topics[topic]
+        return None
+        
 
     def put_topic(self, topic, value):
         """Store in topic the value."""
-        #TODO - verificate if this is correct
+        # topics are in the format --> /weather, /weather/temp, /weather/pressure...
         self._topics[topic] = value
+
 
     def list_subscriptions(self, topic: str) -> List[Tuple[socket.socket, Serializer]]:
         """Provide list of subscribers to a given topic."""
-        #TODO - verificate if this is correct and complete
-        #maybe send something
-        return self.subscribers[topic]
+        return [sub for sub in self.subscribers[topic]]
 
     def subscribe(self, topic: str, address: socket.socket, _format: Serializer = None):
         """Subscribe to topic by client in address."""
-        #TODO - verificate if this is correct and complete
         # Mensagem de broker -> cliente  é em xml ou pickle
         # Mensagem de produtor -> broker  é em json
-        
-        if topic in self.subscribers:
-            self.subscribers[topic].append((address, _format))
-        else:
-            self.subscribers[topic] = [(address, _format)]
 
-        # caso tenha valor no topic (uma publicação) envia a ultima publicação a esse consumidor
-        if topic in self._topics:
-            address.send(self._topics[topic])
+        # get all the child that this topic has
+        subs = [t for t in self.list_topics() if topic in t]
+
+        # and subscribe
+        for t in subs:
+            if t in self._topics.keys():
+                if t in self.subscribers:
+                    self.subscribers[t].append((address, _format))
+                else:
+                    self.subscribers[t] = [(address, _format)]
         
-        # self.SubMesssage(Message) ou algo assim
+        Protocol.send_msg(address, Protocol.reply(topic, self.get_topic(topic)), _format) # sends the last message to the subscriber
+
+        
 
 
 
     def unsubscribe(self, topic, address):
         """Unsubscribe to topic by client in address."""
-        #TODO - verificate if this is correct and complete
 
-        self.subscribers[topic].remove(address)
+        for sub in self.subscribers[topic]:
+            if sub[0] == address: self.subscribers[topic].remove(sub)
         
         # self.CancelSubMesssage(Message) ou algo assim
 
@@ -85,4 +149,6 @@ class Broker:
         """Run until canceled."""
 
         while not self.canceled:
-            pass
+            for key, mask in self.selector.select():
+                callback = key.data
+                callback(key.fileobj, mask)
